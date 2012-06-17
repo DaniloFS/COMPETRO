9d2a8f9cf9561dcbc0b08446d3a05287daa3a17e
santosary <aryfasa@gmail.com>
2012-06-17 11:27:42 -0300

    projeto cliente de aplicativo corporativo

---
+++ ClienteCompetro/src/java/clientecompetro/Main.java
@@ -0,0 +1,16 @@
+package clientecompetro;
+
+/**
+ * @author Danilo
+ * @author Hugo
+ * @author Ary
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
