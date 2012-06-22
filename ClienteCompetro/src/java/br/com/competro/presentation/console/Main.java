41a8a6f119df629e2f18657c1247ebcad41383f7
Danilo Fernandes <danilophernandys@hotmail.com>
2012-06-22 20:17:30 -0300

    Concluido Cliente de aplicativo coorporativo de testes concluido.

---
+++ ClienteCompetro/src/java/br/com/competro/presentation/console/Main.java
@@ -0,0 +1,73 @@
+package br.com.competro.presentation.console;
+
+
+import br.com.competro.domainModel.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Danilo
+ * @author Hugo
+ * @author Ary
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        try {
+            Context context = new InitialContext();
+            ClienteRepository clienteEjb = (ClienteRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/ClienteDAO");
+            ProdutoRepository produtoEjb =(ProdutoRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/ProdutoDAO");
+            FornecedorRepository fornecedorEjb = (FornecedorRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/FornecedorDAO");
+            CompraRepository compraEjb = (CompraRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/CompraDAO");
+            VendaRepository vendaEjb = (VendaRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/CompraDAO");
+            UsuarioRepository usuarioEjb = (UsuarioRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/UsuarioDAO");
+            EnderecoRepository enderecoEjb = (EnderecoRepository) context.lookup("java:global/ClienteCompetro/Competro-ejbPU/EnderecoDAO");
+            
+            Cliente cliente = new Cliente();
+            cliente.setNome("Ariolano");
+            cliente.setCpf("1212324354");
+            clienteEjb.salvar(cliente);
+            
+            Produto produto = new Produto();
+           
+            produto.setDescricao("Oleo para motor");
+            produto.setValorUnitario(2);
+            produtoEjb.salvar(produto);
+            
+            Fornecedor fornecedor = new Fornecedor();
+            fornecedor.setNome("Petrobraz");
+            fornecedor.setCnpj("412514265234");
+            fornecedorEjb.salvar(fornecedor);
+            
+            Usuario usuario = new Usuario();
+            usuario.setLogin("hugo");
+            usuario.setSenha("123");
+            usuario.setNome("Hugo Santos");
+            usuarioEjb.salvar(usuario);
+            
+            Compra compra = new Compra();
+            
+            Venda venda = new Venda();
+            
+            Endereco endereco = new Endereco();
+            endereco.setNumero("123");
+            endereco.setRua("nove");
+            endereco.setBairro("dez");
+            endereco.setCidade("Montes Claros");
+            endereco.setCep("21323621");
+            enderecoEjb.salvar(endereco);
+            
+            
+        } catch (NamingException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
+        }
+        // TODO code application logic here
+    }
+}
